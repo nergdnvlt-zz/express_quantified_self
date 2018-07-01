7bb4fafa9aecdf66230a7ea63ba687221de275c3
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-01 10:31:00 -0600

    Adding base get all routes for meals and foods.

--- api/v1/foods.js
+++ api/v1/foods.js
@@ -1,13 +1,50 @@
 const express = require('express');
-
 const foodRouter = express.Router();
 
-foodRouter.get('/', (req, res) => {
-  res.json({
-    message: 'working'
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../../knexfile')[environment];
+const database = require('knex')(configuration);
+
+foodRouter.get('/', function(req, res, next) {
+  database.raw('SELECT * FROM foods').
+  then(function(foods) {
+    if(!foods.rows) {
+      return res.sendStatus(404);
+    } else {
+     return res.json(foods.rows);
+    }
   })
 });
 
+foodRouter.get('/:id', function(req, res, next) {
+  var id = req.params.id
+  database.raw('SELECT * FROM foods WHERE id = ?', [id]).
+  then(function(foods) {
+    if(!foods.rows) {
+      return res.sendStatus(404);
+    } else {
+     return res.json(foods.rows);
+    }
+  })
+});
+
+foodRouter.post('/', function(req, res, next) {
+  var name = req.body.name
+  var calories = req.body.calories
+  console.log(name)
+  console.log(calories)
+
+  if(!name && !calories) {
+    return res.status(400).send({
+      error: "Please provide both name and calories"
+    })
+  } else {
+    database.raw('INSERT INTO foods (name, calories) VALUES (?,?) RETURNING *', [name, calories]).
+    then(function(foods) {
+      res.status(201).json(foods.rows[0])
+    })
+  }
+})
 
 
 module.exports = foodRouter;
---
+++ db/queries.js
@@ -0,0 +1,10 @@
+// // const knex = require('./knex'); //connection not knex directly
+// const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('../knexfile')[environment];
+// const database = require('knex')(configuration);
+//
+// module.exports = {
+//   getAll(table) {
+//     return database.raw('SELECT * FROM ?', [table])
+//   }
+// }
