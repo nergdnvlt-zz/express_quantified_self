e2bd9443a9b5c9ba3cc5c72aab19a33066c321a6
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-02 15:35:15 -0600

    Adding foodsController, keeping old code.

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -3,6 +3,16 @@ const Food = require('../../models/food')
 
 class foodsController {
 
+  static index(req, res, next) {
+    Food.all()
+    .then(foods => {
+      if(!foods) {
+        return res.sendStatus(404);
+      } else {
+        return res.json(foods);
+      }
+    })
+  }
 // //Get All
 // foodRouter.get('/', function(req, res, next) {
 //   Food.all()
@@ -14,8 +24,19 @@ class foodsController {
 //     }
 //   });
 // });
-//
-//
+
+  static show(req, res, next) {
+    let foodId = req.params.id
+
+    Food.find(foodId)
+    .then(food => {
+      if(!food || food.length < 1) {
+        return res.sendStatus(404);
+      } else {
+       return res.json(food);
+      }
+    });
+  }
 // //Get Single Route
 // foodRouter.get('/:id', function(req, res, next) {
 //   let foodId = req.params.id
@@ -28,8 +49,21 @@ class foodsController {
 //     }
 //   });
 // });
-//
-//
+
+  static create(res, req, next) {
+    let food = req.body.food
+
+    if(!food.name || !food.calories) {
+      return res.status(400).send({
+        error: "Please provide both name and calories"
+      })
+    } else {
+      Food.create(food)
+      .then(food => {
+        res.status(201).json(food)
+      });
+    }
+  }
 // //Create Route
 // foodRouter.post('/', function(req, res, next) {
 //   let food = req.body.food
@@ -45,8 +79,21 @@ class foodsController {
 //     });
 //   }
 // });
-//
-//
+
+  static update(req, res, next) {
+    let food = {id: req.params.id, name: req.body.food.name, calories: req.body.food.calories}
+
+    if(!food.name || !food.calories) {
+      return res.status(400).send({
+        error: "Please provide both name and calories"
+      });
+    } else {
+      Food.update(food)
+      .then(food => {
+        res.status(201).json(food)
+      });
+    }
+  }
 // //Update route
 // foodRouter.patch('/:id', function(req, res, next) {
 //   let food = {id: req.params.id, name: req.body.food.name, calories: req.body.food.calories}
@@ -62,8 +109,23 @@ class foodsController {
 //     });
 //   }
 // });
-//
-//
+
+  static destroy(req, res, next){
+    let idForDeletion = req.params.id
+    Food.destroy(idForDeletion)
+    .then(food => {
+      if(!food){
+        res.status(404).json({
+          error: 404,
+          message: 'There is no food with that ID'
+        });
+      } else {
+        res.status(201).json({
+          message: `Deleted food with id of ${idForDeletion}`
+        });
+      }
+    }
+  }
 // // Delete Path
 // foodRouter.delete('/:id', function(req, res, next) {
 //   let idForDeletion = req.params.id
