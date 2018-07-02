1a822876b5095e965b8b8bc0c43bba3dd29fb94a
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-02 15:21:57 -0600

    Building out foods controller, added functionality to routes.

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -1,31 +1,8 @@
-const express = require('express');
-const foodRouter = express.Router();
-
-const foodsController = require('../../controllers/foodsController')
-
-// Restful API Routes for foods
-
-// Gets all foods
-foodRouter.get('/', foodsController.index)
-
-// Gets single food
-foodRouter.get('/:id', foodsController.show)
-
-// Creates new food
-foodRouter.post('/', foodsController.create)
-
-// Updates current food
-foodRouter.patch('/:id', foodsController.update)
-
-// Deletes Existing Food
-foodRouter.delete('/:id', foodsController.destroy)
+const Food = require('../../models/food')
 
 
-module.exports = foodRouter;
+class foodsController {
 
-// const Food = require('../../models/food')
-//
-//
 // //Get All
 // foodRouter.get('/', function(req, res, next) {
 //   Food.all()
@@ -112,5 +89,6 @@ module.exports = foodRouter;
 //     });
 //   }
 // });
-//
-//
+}
+
+module.exports = foodRouter;
