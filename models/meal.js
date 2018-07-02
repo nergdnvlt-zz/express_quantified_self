8f0a138b73c07b6924b53075eda723a9ac32ca2c
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-02 11:18:23 -0600

    Organizing classes.

--- models/meal.js
+++ models/meal.js
@@ -7,21 +7,21 @@ class Meal {
     return database('meals').select('id', 'name').map(this.getFoods)
   }
 
-    static find(id){
-      return database('meals').where('id', id).select('id', 'name').map(this.getFoods)
-      .then(rows => rows[0])
-    }
-
-    static getFoods(meal){
-      return database('foods')
-      .select('foods.id', 'foods.name', 'foods.calories')
-      .join('mealfoods', {'foods.id': 'mealfoods.food_id'})
-      .where('mealfoods.meal_id', meal.id)
-      .then(foods => {
-        meal.foods = foods
-        return meal
-      })
-    }
+  static find(id){
+    return database('meals').where('id', id).select('id', 'name').map(this.getFoods)
+    .then(rows => rows[0])
   }
+
+  static getFoods(meal){
+    return database('foods')
+    .select('foods.id', 'foods.name', 'foods.calories')
+    .join('mealfoods', {'foods.id': 'mealfoods.food_id'})
+    .where('mealfoods.meal_id', meal.id)
+    .then(foods => {
+      meal.foods = foods
+      return meal
+    })
+  }
+}
 
 module.exports = Meal
