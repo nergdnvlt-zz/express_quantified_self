5bc7761103738af9dc109b3e785e31372b30e049
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-02 19:11:35 -0600

    Adding cascading delete to migrations.

--- db/migrations/20180628154209_create-mealfoods-table.js
+++ db/migrations/20180628154209_create-mealfoods-table.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   let createQuery = `CREATE TABLE mealfoods(
     id SERIAL PRIMARY KEY NOT NULL,
-    meal_id integer REFERENCES meals (id),
-    food_id integer REFERENCES foods (id)
+    meal_id integer REFERENCES meals (id) ON DELETE CASCADE,
+    food_id integer REFERENCES foods (id) ON DELETE CASCADE
   )`
   return knex.raw(createQuery)
 };
